c3a94b9c8784e48fabb6d655405d8dc715a7de67
Anirban Saha <anirban.saha.1996@gmail.com>
2020-09-13 23:34:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,64 +1,14 @@
 import streamlit as st
-import bs4 as bs  
-import urllib.request  
-import re
 
 def main():
-    st.title("Wikipedia Summarizer")
-    url_topull= st.text_input("Enter the Wikipedia URL to pull - ")
-    if url_topull!='':
-        scraped_data = urllib.request.urlopen(url_topull)  
-        article = scraped_data.read()
-
-        parsed_article=bs.BeautifulSoup(article,'lxml')
-
-        paragraphs = parsed_article.find_all('p')
-
-        article_text = ""
-
-        for p in paragraphs:  
-            article_text += p.text
-        article_text = re.sub(r'\[[0-9]*\]', ' ', article_text)  
-        article_text = re.sub(r'\s+', ' ', article_text)
-
-        import nltk
-        nltk.download('punkt')
-        nltk.download('stopwords')
-        import heapq
-        number=st.text_input('How many sentences long do you want your summary to be?')
-        if number!='':    
-            sent_num = int(number)
-            formatted_article_text = re.sub('[^a-zA-Z]', ' ', article_text )  
-            formatted_article_text = re.sub(r'\s+', ' ', formatted_article_text)
-            sentence_list = nltk.sent_tokenize(article_text)  
-
-            stopwords = nltk.corpus.stopwords.words('english')
-            word_frequencies = {}  
-            for word in nltk.word_tokenize(formatted_article_text):  
-                if word not in stopwords:
-                    if word not in word_frequencies.keys():
-                        word_frequencies[word] = 1
-                    else:
-                        word_frequencies[word] += 1
-
-            maximum_frequncy = max(word_frequencies.values())
-
-            for word in word_frequencies.keys():  
-                word_frequencies[word] = (word_frequencies[word]/maximum_frequncy)
-            sentence_scores = {}  
-            for sent in sentence_list:  
-                for word in nltk.word_tokenize(sent.lower()):
-                    if word in word_frequencies.keys():
-                        if len(sent.split(' ')) < 30:
-                            if sent not in sentence_scores.keys():
-                                sentence_scores[sent] = word_frequencies[word]
-                            else:
-                                sentence_scores[sent] += word_frequencies[word]
-
-            summary_sentences = heapq.nlargest(sent_num, sentence_scores, key=sentence_scores.get)
-            summary = ' '.join(summary_sentences)  
-            st.markdown("# Summary: ")
-            st.write(summary)   
+    st.title("Final Grade Required")
+    sem1= st.number_input("Enter the first semester GPA - ")
+    sem2= st.number_input("Enter the second semester GPA - ")
+    sem3= st.number_input("Enter the third semester GPA - ")
+    target= st.number_input("Enter the target CGPA - ")
+    if target!=0:
+        required = (target*82 - 19*sem1 - 23*sem2 - 20*sem3)/20;
+        st.write("The GPA required in your fourth semester is : ", required)   
     
 if __name__ == '__main__':
     main()
